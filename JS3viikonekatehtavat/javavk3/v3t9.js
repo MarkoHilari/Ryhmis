a9e2322d95d2c39eac00be0e9fadf0297b7ae3fa
Mika Huotari <mika.huotari@edu.keuda.fi>
2022-11-02 11:19:40 +0200

    paskaa

--- JS3viikonekatehtavat/javavk3/v3t9.js
+++ JS3viikonekatehtavat/javavk3/v3t9.js
@@ -1,5 +1,19 @@
-fuction etsi()
+/*function etsi()
 {
-  var luku = document.getElementById('luku').value;
-  luku = Math.max
+  let numero = []
+  numero[0] = document.getElementById('num1').value;
+  numero[1] = document.getElementById('num2').value;
+  numero[2] = document.getElementById('num3').value;
+  numero[3] = document.getElementById('num4').value;
+  numero[4] = document.getElementById('num5').value;
+
+    console.log(numero[0]);
+
+}*/
+
+function etsi()
+{
+  let num1 = document.getElementById('num1').value;
+ let x = Math.max(num1, num2, num3, num4, num5);
+  console.log(etsi);
 }
