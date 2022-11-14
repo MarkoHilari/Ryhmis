b8ecb9af8a01b6c88f8ed7678f6e0c5c3e9ffe51
MarkoHilari <marko.hilari@edu.keuda.fi>
2022-11-14 11:33:46 +0200

    Jotain vikaa

--- parallax muuttuva/muuttuva.js
+++ parallax muuttuva/muuttuva.js
@@ -3,4 +3,11 @@ function aani(sound)
   aani = sound.id;
   var audio = new Audio(aani+'.mp3');
   audio.play();
+
+}
+
+function stop(sound){
+  aani2 = sound.id;
+  var audio = new Audio(aani2+'.mp3');
+  audio.stop();
 }
