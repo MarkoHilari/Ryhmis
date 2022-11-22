6edcee92308ad92a21f3282a61e301ffa4f5b63a
MarkoHilari <marko.hilari@edu.keuda.fi>
2022-11-22 09:36:32 +0200

    kug,mhuik

--- parallax muuttuva/muuttuva.js
+++ parallax muuttuva/muuttuva.js
@@ -3,8 +3,12 @@ function aani(sound)// Tästä tulee äänet napille
   aani = sound.id;
   var audio = new Audio(aani+'.mp3');
   audio.play();
+  const myTimeout = setTimeout(timer, 6000);
 }
+function timer() {
+  location.reload();
 
+}
 /*function stop(sound)// Tämä ei toimi
 {
   aani = sound.id;
